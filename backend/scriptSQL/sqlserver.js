d6d2ed52400f5de568d716ae5d284c059681c78b
angel gonzalez <angel.gonzalez@zascita.com>
2024-02-09 18:02:04 -0600

    conexion a sqlserve y validacion de rutas

---
+++ backend/scriptSQL/sqlserver.js
@@ -0,0 +1,19 @@
+const { getConnection, mssql } = require('../database/conexionSqlServer');
+
+const getUsuarios = async () => {
+    try {
+        const pool = await getConnection();
+        if (pool) {
+            console.log('Objeto pool devuelto correctamente');
+            const result = await pool.request().query("SELECT prv,role FROM Users");
+            // console.log(result.recordset);
+            console.table(result.recordset)
+        } else {git 
+            console.error('Error: Objeto pool no devuelto');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getUsuarios();
