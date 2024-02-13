94ab44c3c05094adf07651e7184f5256a73caa76
angel gonzalez <angel.gonzalez@zascita.com>
2024-02-13 13:08:52 -0600

    funcionalidad de boton menu sliderbar

--- front/src/app/components/dashboard/dashboard.component.ts
+++ front/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const sidebar = document.querySelector('.sidebar');
+    const closeBtn = document.querySelector('#btn');
+    const searchBtn = document.querySelector('.bx-search');
+
+    if (closeBtn) {
+      closeBtn.addEventListener('click', () => {
+        if (sidebar) {
+          sidebar.classList.toggle('open');
+          menuBtnChange();
+        }
+      });
+    }
+
+    if (searchBtn) {
+      searchBtn.addEventListener('click', () => {
+        if (sidebar) {
+          sidebar.classList.toggle('open');
+          menuBtnChange();
+        }
+      });
+    }
+
+    function menuBtnChange() {
+      if (sidebar && closeBtn) {
+        if (sidebar.classList.contains('open')) {
+          closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right');
+        } else {
+          closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu');
+        }
+      }
+    }
+  }
+
+
 
 }
