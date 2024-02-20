af0e143f41a606b2d17313e4a3065cd5f7ebcc83
angel gonzalez <angel.gonzalez@zascita.com>
2024-02-20 14:27:18 -0600

    creaciond de "seccion" graficas y exportar

---
+++ front/src/app/components/panelgraficas/panelgraficas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panelgraficas',
+  templateUrl: './panelgraficas.component.html',
+  styleUrls: ['./panelgraficas.component.css']
+})
+export class PanelgraficasComponent {
+
+}
