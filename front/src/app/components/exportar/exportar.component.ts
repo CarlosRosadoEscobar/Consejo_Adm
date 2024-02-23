875d11d3a934da26775e225b5abf3c63ee0419d6
AlexisMG2020 <127353210+AlexisMG2020@users.noreply.github.com>
2024-02-23 16:57:45 -0600

    PDF

--- front/src/app/components/exportar/exportar.component.ts
+++ front/src/app/components/exportar/exportar.component.ts
@@ -8,29 +8,36 @@ import { PdfService } from 'src/app/services/pdf.service';
   styleUrls: ['./exportar.component.css']
 })
 export class ExportarComponent {
- 
-  listDocumentos : Documentos[] = [];
+  listDocumentos: Documentos[] = [];
+  subfilaVisible: { [key: number]: boolean } = {};
+  isLoading: boolean = true; // Variable para indicar si la carga está en curso
 
-  constructor(
-    private _documentoService : PdfService,
-  ){}
+  constructor(private _documentoService: PdfService) {}
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.obtenerDocumentos();
   }
 
-  obtenerDocumentos(){
-    this._documentoService.obtenerDocumentos().subscribe(data =>{
-      console.log(data);     
-      return this.listDocumentos = data;     
-    },error=>{
+  obtenerDocumentos() {
+    this.isLoading = true; // Inicia el spinner
+    this._documentoService.obtenerDocumentos().subscribe(data => {
+      console.log(data);
+      this.listDocumentos = data;
+      this.isLoading = false;
+    }, error => {
       console.log(error);
-    });    
+      this.isLoading = false; 
+    });
   }
 
-
-
-
-
-  
+  obtenerDocumento(id: number): void {
+    if (this.subfilaVisible[id]) {
+      this.subfilaVisible[id] = false;
+    } else {
+      Object.keys(this.subfilaVisible).forEach((key: string) => {
+        this.subfilaVisible[+key] = false;
+      });  
+      this.subfilaVisible[id] = true;
+    }
+  }
 }
