ed387ab6a2d2205e8c73eca83f9ad3426031d765
AlexisMG2020 <127353210+AlexisMG2020@users.noreply.github.com>
2024-02-28 16:09:09 -0600

    PDF

--- front/src/app/models/documentos.ts
+++ front/src/app/models/documentos.ts
@@ -2,9 +2,11 @@ export class Documentos {
     id:number;
     documento:string;
     fecha:string;
-    constructor(id:number,documento:string,fecha:string){
+    usuario:string
+    constructor(id:number,documento:string,fecha:string,usuario:string){
         this.id=id;
         this.documento=documento;
         this.fecha=fecha;
+        this.usuario=usuario;
     }
 }
