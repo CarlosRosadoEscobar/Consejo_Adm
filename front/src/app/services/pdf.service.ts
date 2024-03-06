25af92459a9a37b41d488d2db5c8995f1a5c979a
AlexisMG2020 <127353210+AlexisMG2020@users.noreply.github.com>
2024-03-06 17:22:52 -0600

    pDF firma

--- front/src/app/services/pdf.service.ts
+++ front/src/app/services/pdf.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Documentos } from '../models/documentos';
@@ -18,9 +18,20 @@ export class PdfService {
     return this.http.get(this.url);
   }
 
-  guardarDocumento(documento : Documentos) : Observable<any>{
+  /* guardarDocumento(documento : Documentos) : Observable<any>{
     return this.http.post(this.url,documento);
+  } */
+
+  guardarDocumento(documento: Documentos): Observable<any> {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    return this.http.post(this.url, documento, options);
   }
+
 
   obtenerDocumento(id: number): Observable<any> {
     return this.http.get(this.url + id);
