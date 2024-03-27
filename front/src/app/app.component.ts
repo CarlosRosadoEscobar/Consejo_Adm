0a7cfb59a9daf6b66ef7e5ec8fbf00f2ae6768b5
angel gonzalez <angel.gonzalez@zascita.com>
2024-03-26 19:12:21 -0600

    componente usuarios

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { Router, Event, NavigationEnd } from '@angular/router';
+
+import { IStaticMethods } from 'preline/preline';
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
 
 @Component({
@@ -10,8 +18,20 @@ import { initFlowbite } from 'flowbite';
 export class AppComponent implements OnInit {
   title = 'front';
 
+  constructor(
+    private router: Router
+  ) {}
+
+
   ngOnInit(): void {
     initFlowbite();
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          window.HSStaticMethods.autoInit();
+        }, 100);
+      }
+    });
   }
-  
+
 }
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
     "./node_modules/flowbite/**/*.js",
+    './node_modules/preline/preline.js',
   ],
   theme: {
     extend: {},
@@ -11,7 +12,19 @@ module.exports = {
     require('flowbite/plugin'),
     require('flowbite/plugin')({
       charts: true,
-  }),
+    }),
+    require('preline/plugin'),
   ],
+  colors: {
+    'blue': '#1fb6ff',
+    'purple': '#7e5bef',
+    'pink': '#ff49db',
+    'orange': '#ff7849',
+    'green': '#13ce66',
+    'yellow': '#ffc82c',
+    'gray-dark': '#273444',
+    'gray': '#8492a6',
+    'gray-light': '#d3dce6',
+  },
 }
 
