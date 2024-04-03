cffdad34b3a44abaa5b06f029dc9b9c817682b30
angel gonzalez <angel.gonzalez@zascita.com>
2024-04-03 17:08:35 -0600

    quitando libreria pdf

--- front/src/app/components/documentostodos/documentostodos.component.ts
+++ front/src/app/components/documentostodos/documentostodos.component.ts
@@ -1,7 +1,5 @@
-import { Component, ViewChild, ElementRef, OnDestroy  } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { NgxExtendedPdfViewerService } from 'ngx-extended-pdf-viewer';
-import * as pdfjs from 'pdfjs-dist';
+import { Component, OnDestroy  } from '@angular/core';
+
 
 
 @Component({
@@ -12,9 +10,6 @@ import * as pdfjs from 'pdfjs-dist';
   styleUrl: './documentostodos.component.css'
 })
 export class DocumentostodosComponent implements OnDestroy {
-
-  @ViewChild('pdfViewer', {static: true}) pdfViewer!: ElementRef<HTMLCanvasElement>;
-  pdfSrc = '../../../assets/documents/plan.pdf';
 
   constructor() {
     const pageReloaded = localStorage.getItem('pageReloaded');
@@ -27,37 +22,6 @@ export class DocumentostodosComponent implements OnDestroy {
   ngOnDestroy(): void {
     localStorage.removeItem('pageReloaded');
   }
-
-  ngAfterViewInit(): void {
-    this.showPdf();
-  }
-
-  showPdf(): void {
-    const loadingTask = pdfjs.getDocument(this.pdfSrc);
-    loadingTask.promise.then(pdf => {
-      pdf.getPage(1).then(page => {
-        const scale = .5;
-        const viewport = page.getViewport({ scale });
-
-        const canvas = this.pdfViewer.nativeElement;
-        const context = canvas.getContext('2d');
-
-        if (context) { // Verificar si context no es nulo
-          canvas.height = viewport.height;
-          canvas.width = viewport.width;
-
-          const renderContext = {
-            canvasContext: context,
-            viewport: viewport
-          };
-          page.render(renderContext);
-        } else {
-          console.error("Error: Canvas context is null");
-        }
-      });
-    });
-  }
-
 
 
 }
