afe8d95ff8752c377528d280c81942bef95edd05
AlexisMG2020 <127353210+AlexisMG2020@users.noreply.github.com>
2024-04-18 13:12:41 -0600

    formulario usuarios

--- frontend/src/app/modules/modulos/administrador/admin-usuarios-registro/admin-usuarios-registro.component.ts
+++ frontend/src/app/modules/modulos/administrador/admin-usuarios-registro/admin-usuarios-registro.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
+import { CargoService } from '../../../../data/dataCargo';
+import { Estados } from '../../../../data/dataRegion';
+import { Pregunta,PreguntasService } from '../../../../data/dataPreguntas';
+import { RolesService } from '../../../../data/dataRol';
 
 @Component({
   selector: 'app-admin-usuarios-registro',
   templateUrl: './admin-usuarios-registro.component.html',
-  styleUrl: './admin-usuarios-registro.component.css'
+  styleUrls: ['./admin-usuarios-registro.component.css']
 })
+
 export class AdminUsuariosRegistroComponent {
+  areas: string[] = [];
+  areaSeleccionada: string = '';
+  puestos: any[] = [];
+  puestoSeleccionado:string = '';
+  
+  estados: any[] = Estados.estados;
+  estadoSeleccionado: string='';
+  
+  preguntaSeleccionada: number | null = null;
+  preguntas: Pregunta[] = PreguntasService.preguntas;
+  respuesta: string | null = null;
+  @ViewChild('respuestaInput') respuestaInput: any;
+
+
+  roles: any[] = RolesService.roles;
+  rolSeleccionado: string = '';
+
+  
+  constructor(private cargoService: CargoService) {
+    this.areas = Object.keys(this.cargoService.getCargosPorArea());
+    console.log(this.roles);
+    
+  }
+  
+  cargarPuestos(): void {
+    if (this.areaSeleccionada) {
+      this.puestos = this.cargoService.getCargosPorArea()[this.areaSeleccionada];
+      console.log(this.puestos);
+    } else {
+      this.puestos = [];
+    }
+  }
+  
+  resetRespuesta() {
+    this.respuesta = null; // Reinicia la respuesta a null
+    if (this.respuestaInput) {
+      this.respuestaInput.nativeElement.value = ''; // Restablece el valor del input de respuesta
+    }
+  }
+  
+  mostrarInputRespuesta(): boolean {
+    return this.preguntaSeleccionada !== null;
+  }
 
 }
